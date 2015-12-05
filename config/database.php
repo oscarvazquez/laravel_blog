c523c8c2fe4086d007a906ec13c65152d39a1950
oscarvazquez <oscar.vazquez2012@gmail.com>
2015-12-04 22:09:39 -0800

    changed sqlite to mysql

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'oscar'),
+			'username'  => env('DB_USERNAME', 'root'),
+			'password'  => env('DB_PASSWORD', 'root'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
@@ -67,8 +67,8 @@ return [
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
+			'database' => env('DB_DATABASE', 'oscar_tech'),
+			'username' => env('DB_USERNAME', 'oscar_tech'),
 			'password' => env('DB_PASSWORD', ''),
 			'charset'  => 'utf8',
 			'prefix'   => '',
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -23,7 +23,6 @@
 						<li><a class = "color_white" href="/projects/create">Create Project</a></li>
 						<li><a class = "color_white" href="/messages">Messages</a></li>
 					@endif
-<!-- 					<li><a class = "color_white" href="/articles/{{$latest->id}}">{{$latest->title}}</a></li> -->
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right something">
