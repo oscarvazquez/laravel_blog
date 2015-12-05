6b7f7a52f74b30a83a741cca397c4515ccb00344
root <root@ip-172-31-27-126.us-west-2.compute.internal>
2015-12-05 10:09:51 +0000

    production ready

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
 	|
 	*/
 
-	'debug' => env('APP_DEBUG'),
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
 	|
 	*/
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
