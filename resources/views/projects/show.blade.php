aa026fd4e0634df5cb08764469b24b5cca140917
oscarvazquez <oscar.vazquez2012@gmail.com>
2015-12-08 17:15:22 -0800

    I fixed project show, switched description with body and the way the inforamtion is being presented projects/show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 <?php 
-	$pro = html_entity_decode($project->body);
+	$pro = html_entity_decode($project->description);
 ?>
 	<div class = "container-fluid">
 		<div class = "col-lg-7 col-md-7 col-sm-12 col-xs-12">
+			<?= html_entity_decode($project->body) ?>
 		</div>
 		<div class = "col-lg-5 col-md-5 col-sm-12 col-xs-12">
 			<h1>{{$project->title}}</h1>
